2214d36dcf3da185e67efd19dda94309b5a42b29
Boris Skert <boris.skert@gmail.com>
2025-01-23 21:12:06 +0100

    Try to improve multiply algorithm

--- biginteger/multiply.go
+++ biginteger/multiply.go
@@ -5,25 +5,16 @@ func multiplyAbs(a BigInteger, b BigInteger) BigInteger {
 	b = b.Abs()
 
 	result := Zero
-	factor := One
 	multiplier := a
 	remaining := b
 
-	for {
-		if remaining.IsGreaterThan(factor) {
+	for remaining.IsGreaterThan(Zero) {
+		if remaining.IsOdd() {
 			result = result.Add(multiplier)
-			remaining = remaining.Subtract(factor)
-			multiplier = multiplier.Add(multiplier)
-			factor = factor.Add(factor)
-		} else if remaining.IsEqualTo(factor) {
-			result = result.Add(multiplier)
-			break
-		} else if remaining.IsLessThan(factor) {
-			factor = One
-			multiplier = a
-		} else {
-			break
 		}
+
+		multiplier = multiplier.Add(multiplier)
+		remaining = remaining.ShiftRight(One)
 	}
 
 	return result
