9eed99299b4f7b716cebca745757cdb419e940db
Boris Skert <boris.skert@byteagenten.de>
2025-02-04 13:06:05 +0100

    Extend tests

--- digits/digits_test.go
+++ digits/digits_test.go
@@ -65,4 +65,61 @@ var _ = Describe("Digits", func() {
 			Expect(result.DigitAt(1)).To(Equal(Digit(18446744073709551615)))
 		})
 	})
+
+	Context("MultiplyByDoubleDigit", func() {
+		It("1 * 1", func() {
+			a := Digit(1).AsDigits()
+			b := Digit(1).AsDoubleDigit()
+
+			result := a.MultiplyByDoubleDigit(b)
+
+			Expect(result.Length()).To(Equal(uint(1)))
+			Expect(result.DigitAt(0)).To(Equal(Digit(1)))
+		})
+
+		It("2 * 2", func() {
+			a := Digit(2).AsDigits()
+			b := Digit(2).AsDoubleDigit()
+
+			result := a.MultiplyByDoubleDigit(b)
+
+			Expect(result.Length()).To(Equal(uint(1)))
+			Expect(result.DigitAt(0)).To(Equal(Digit(4)))
+			Expect(result.DigitAt(1)).To(Equal(Digit(0)))
+		})
+
+		It("2 pow 64 * 2 pow 64", func() {
+			a := Digit(1).AsDigits().LeftShiftBits(64)
+			b := Digit(1).AsDigits().LeftShiftBits(64).AsDoubleDigit()
+
+			result := a.MultiplyByDoubleDigit(b)
+
+			Expect(result.Length()).To(Equal(uint(3)))
+			Expect(result.DigitAt(0)).To(Equal(Digit(0)))
+			Expect(result.DigitAt(1)).To(Equal(Digit(0)))
+			Expect(result.DigitAt(2)).To(Equal(Digit(1)))
+		})
+
+		It("2 pow 64 * 2 pow 64 - 1", func() {
+			a := Digit(1).AsDigits().LeftShiftBits(64)
+			b, _ := Digit(1).AsDigits().LeftShiftBits(64).Decrement()
+
+			result := a.MultiplyByDoubleDigit(b.AsDoubleDigit())
+
+			Expect(result.Length()).To(Equal(uint(2)))
+			Expect(result.DigitAt(0)).To(Equal(Digit(0)))
+			Expect(result.DigitAt(1)).To(Equal(Digit(18446744073709551615)))
+		})
+
+		It("(2 pow 64) * (2 pow 64)", func() {
+			a, _ := Digit(1).AsDigits().LeftShiftBits(64).Decrement()
+			b, _ := Digit(1).AsDigits().LeftShiftBits(64).Decrement()
+
+			result := a.MultiplyByDoubleDigit(b.AsDoubleDigit())
+
+			Expect(result.Length()).To(Equal(uint(2)))
+			Expect(result.DigitAt(0)).To(Equal(Digit(1)))
+			Expect(result.DigitAt(1)).To(Equal(Digit(18446744073709551614)))
+		})
+	})
 })
